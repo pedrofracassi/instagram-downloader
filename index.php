e62797453bbde1f118daa5bf04f0de484eba3d23
pedroluizfracassi <pedroluizfracassi@gmail.com>
2017-01-18 12:44:40 -0500

    Added bootstrap

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
-<html>
-    <head>
-        <title>Ayy!</title>
-    </head>
-    <body>
-        <h1>Yay, GitHub!</h1>
-    </body>
+<html lang="en-US">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Instagram Downloader</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+</head>
+
+<body>
+    <h1>Yay, GitHub!</h1>
+</body>
+
 </html>
